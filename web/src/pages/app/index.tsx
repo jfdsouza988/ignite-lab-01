53db750f75949e7ae49c656fe45754ca03706e63
jfdsouza988 <juliosouza.rs@gmail.com>
2022-04-12 00:03:24 -0300

    integracao front-end e back-end

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,17 +1,38 @@
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
-import Link from "next/link";
+import { withApollo } from "../../lib/withApollo";
+import { useGetProductsQuery, useMeQuery } from "../../graphql/generated/graphql";
+import { getServerPageGetProducts, ssrGetProducts } from "../../graphql/generated/page";
 
-export default function Home() {
+function Home({ data }) {
   const { user } = useUser();
+  const { data: me } = useMeQuery();
 
   return (
-    <div>
+    <div className="text-violet-500">
       <h1>Hello World!</h1>
+      <pre>
+        {JSON.stringify(me, null, 2)}
+      </pre>
+
+      {/* <pre>
+        {JSON.stringify(data.products, null, 2)}
+      </pre> */}
 
       <pre>{JSON.stringify(user, null, 2)}</pre>      
     </div>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (ctx) => {    
+    // getServerPageGetProducts({}, ctx);
+    return {
+      props: {}
+    }      
+  }
+});
+
+export default withApollo(
+  ssrGetProducts.withPage()(Home)
+);
